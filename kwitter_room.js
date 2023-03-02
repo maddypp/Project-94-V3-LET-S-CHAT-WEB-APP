9399b35d6c0cd602fe6b368062fce679ebf210d3
maddypp <113092719+maddypp@users.noreply.github.com>
2023-03-02 19:12:38 +1100

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAMYuYZ_0Av6-IjEFMbwh4LlwlFNnrVXFQ",
+    authDomain: "let-s-chat-web-app-8ece7.firebaseapp.com",
+    projectId: "let-s-chat-web-app-8ece7",
+    storageBucket: "let-s-chat-web-app-8ece7.appspot.com",
+    messagingSenderId: "835456606609",
+    appId: "1:835456606609:web:106c3bbf3f0cafd1307a30"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
